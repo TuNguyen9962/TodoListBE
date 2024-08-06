a63e7939138aa1daa3dd858f1c1f3ab30b1c9c51
Quang Minh <onelove4everu@gmail.com>
2024-08-06 14:03:03 +0700

    controller user (update,delete)

--- controllers/User/userController.js
+++ controllers/User/userController.js
@@ -9,3 +9,16 @@ exports.createUser = (req, res) => {
   res.write('Create A User');
   res.end();
 };
+
+
+exports.updateUser = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.write('Update User');
+  res.end();
+}
+
+exports.deleteUser = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.write('Delete User');
+  res.end();
+}
--- routes/users/index.js
+++ routes/users/index.js
@@ -3,4 +3,6 @@ const userController = require('../../controllers/User/userController');
 module.exports = {
   GET: userController.getUser,
   POST: userController.createUser,
+  PUT: userController.updateUser,
+  DELETE: userController.deleteUser,
 };
