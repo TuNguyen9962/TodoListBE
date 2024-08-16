ca80d4a1a18643fda922a2ba7718109f6646217f
Tú Nguyễn Phạm Anh <nguyentu9962@gmail.com>
2024-08-16 22:28:16 +0700

    feat: create funcion run middleware and controller

--- middlewares/check_token.js
+++ middlewares/check_token.js
@@ -1,5 +1,29 @@
 function getUserToken() {
-  return {token : 'This is my token'}
+  return {token : '123456789'}
 }
 
-module.exports = { getUserToken }
+const checkToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  // console.log(token)
+  if (!token) {
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      code: 'UNAUTHORIZED',
+      message: 'Token is missing or invalid',
+    }));
+    return;
+  }
+
+  if (token !== '123456789') {
+    res.writeHead(403, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      code: 'FORBIDDEN',
+      message: 'Token is invalid',
+    }));
+    return;
+  }
+
+  next();  // Token hợp lệ, tiếp tục
+};
+
+module.exports = { getUserToken, checkToken }
