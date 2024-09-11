9f755b02f370901d0a4f8b7b52e0e46ac62cd7e9
Quang Minh <onelove4everu@gmail.com>
2024-09-11 20:29:48 +0700

    Update getUserToken and checkToken to Use JWT

--- middlewares/check_token.js
+++ middlewares/check_token.js
@@ -1,5 +1,13 @@
-function getUserToken() {
-  return {token : '123456789'}
+const jwt = require('jsonwebtoken');
+
+// Secret key for signing JWTs (should be stored securely in environment variables )
+const SECRET_KEY = 'your_secret_key';
+
+// Function to generate a JWT for a user
+function getUserToken( username) {
+  const token = jwt.sign({ username }, SECRET_KEY ); 
+  //const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });  Token expires in 1 hour
+  return { token };  // Return the token to the caller 
 }
 
 const checkToken = (req, res, next) => {
@@ -14,16 +22,21 @@ const checkToken = (req, res, next) => {
     return;
   }
 
-  if (token !== '123456789') {
-    res.writeHead(403, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-      code: 'FORBIDDEN',
-      message: 'Token is invalid',
-    }));
-    return;
-  }
-
-  next();  // Token hợp lệ, tiếp tục
+    // Verify the token
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) {
+        res.writeHead(403, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+          code: 'FORBIDDEN',
+          message: 'Token is invalid',
+        })
+      );
+        return;
+      }
+      req.user = user;  // Store the decoded user info in req.user for further use
+      next();  // Continue to the next middleware or route handler
+    });
 };
 
 module.exports = { getUserToken, checkToken }
